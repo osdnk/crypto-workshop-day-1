515092701912bcaea19d09d59850b2bd10ea36c0
Michał Osadnik <micosa97@gmail.com>
2025-06-11 15:30:31 +0300

    Update params.rs

--- src/params.rs
+++ src/params.rs
@@ -1,6 +1,6 @@
-pub const N: usize = 600; // Dimension of the secret
-pub const Q: u64 = 32741;  // Modulus
-pub const M: usize = 1000; // Number of A matrix rows (more than N)
+pub const N: usize = 10; // Dimension of the secret
+pub const Q: u64 = 17;  // Modulus
+pub const M: usize = 30; // Number of A matrix rows (more than N)
 pub const ERROR_BOUND: u64 = 3; // Small error
 
 pub type VecN = [u64; N];
